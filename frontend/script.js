cccf348d5854112299a523a0cc5f02ed63516a8a
RekaVadasz <vadasz.reka87@gmail.com>
2022-05-10 14:25:27 +0200

    Work on class 0510

--- frontend/script.js
+++ frontend/script.js
@@ -1,87 +1,26 @@
-console.log(varVariable); //lentebb inicializűltuk de már itt is elérhető a mneve, de értéke undefined
 
-let globalVariable1 = 12;
-
-let globalVariable2 = function (){
-    let funVariable1 = "hello"; 
-    
-    console.log(globalVariable1);
-    
-    let funVariable2 = function (){
-        console.log("funVariable2 függvény belseje");
-        console.log(globalVariable1);
-        console.log(funVariable1);
-        
-        let funInFunVariable1 = true;
-        console.log(funInFunVariable1);
-        let funInFunVariable2 = function (){
-            console.log(funInFunVariable1);
-        }
-        funInFunVariable2();
-    }
-    funVariable2();
-
-    console.log(funVariable1);
-    if (funVariable1 === "hi") {
-        var blockVariable1 = "bye";
-    }else {
-        //console.log(blockVariable1) 
-    }
-    console.log(blockVariable1); 
+const menuButtonComponent = function (){
+    return`
+    <button id="menu-btn"></button>
+    `
 }
 
-globalVariable2();
-
-// console.log(funVariable1); ez itt nem látható, mivel egy függvényen belül hoztuk létre
-
-
-
-var varVariable = 1;
-
-let letVariable1;
-console.log(letVariable1);
-
-let letVariable2 = "variable 2";
-console.log(letVariable2);
-
-const constVariable = "ciao";
-//constVariable = "hello";
-console.log(constVariable);
-
-const constObj1 = {};
-// constObj1.key = "value";
-//constObj1 = {key : "value"};
-console.log(constObj1); 
-
-
-const globalVariable3 = function (parameter1, parameter2, parameter3) {
-    console.log(parameter1);
-    console.log(parameter2);
-    console.log(parameter3);
-    console.log(parameter3());
-}
-
-const globalVariable4 = "apple";
-
-const globalVariable5 = function (){
-    return "pear";
-}
-
-const globalVariable6 = function (){
-    return "shoes";
-}
-
-globalVariable3(globalVariable5(), globalVariable4, globalVariable6);
-
-const loadEvent = function (para1){
+const loadEvent = function (){
     const rootElement = document.getElementById("root");
-    console.log(rootElement);
-    console.log(para1);
-    //console.log(globalVariable1);
-    rootElement.addEventListener("click", function (event){
-        console.log(event.currentTarget);
-        event.currentTarget.insertAdjacentHTML("beforeend", "Clicked, ");
-        event.currentTarget.classList.toggle("clicked")
+    console.log(rootElement.parentElement)
+
+    rootElement.insertAdjacentHTML("beforeend", menuButtonComponent());
+
+    const menuButtonElement = document.getElementById("menu-btn");
+
+    menuButtonElement.addEventListener("click", function (event){
+        //event.currentTarget.classList.toggle("clicked");
+        /* console.log(rootElement)
+        console.log(event.currentTarget.parentElement)
+        console.log(event.currentTarget.closest("#root")) */
+
+        event.currentTarget.closest("#root").classList.toggle("menu-opened")
+
     })
 
 }
